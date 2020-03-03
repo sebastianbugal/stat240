3e4fd6156ad6fa7a9d7a43777fd0ae516cda95d8
Sebastian Bugal <seb1001@d207-023-206-073.wireless.sfu.ca>
2020-03-02 16:50:12 -0800

    returning things

--- midterm 2/q3.R
+++ midterm 2/q3.R
@@ -9,19 +9,25 @@ s
 s=gsub('/',' ',s)
 s[[1]]
 
+
 str_detect(s,"clear",negate=TRUE)
 a=strsplit(s," |/")
 a[[1]][1]
 
 m = gregexpr(' \\d+ | \\d+ \\d+', s)
+m
 x = regmatches(s, m)
 x = trimws(x[[1]])
 x
-
+as.integer(x)[2]
 
 translink = function(year, month, day, hour){
   load(file = 'translink.Rdata')
   curtweet=NULL
+  stop=NULL
+  start=NULL
+  returning=NULL
+  
   if(year<0){
     print("Error 1: Invalid value for year")
     return(NULL)
@@ -48,32 +54,45 @@ translink = function(year, month, day, hour){
         curtweet=c(curtweet,i)
     }
   }
-  
   for(i in curtweet){
+    
     i$text=tolower(i$text)
     i$text=gsub('/',' ',i$text)
+    
     if(str_detect(i$text,"detour") & 
-       str_detect(i$text,"clear",negate=TRUE)& str_detect(i$text," over",negate=TRUE)){
+       str_detect(i$text,"clear",negate=TRUE)& 
+       str_detect(i$text," over",negate=TRUE)){
+      
       print("detour")
+      
       i_splt_regroute=strsplit(i$text, "regular route")
       m = gregexpr(' \\d+ | \\d+ \\d+', i_splt_regroute[[1]][1])
       x = regmatches(i_splt_regroute[[1]][1], m)
       x = trimws(x[[1]])
+      start=c(start,as.integer(x))
       print(x)
       print(i$text)
     }
+    
     else{
       print("detour cleared")
+      
       i_splt_regroute=strsplit(i$text, "regular route")
       m = gregexpr(' \\d+ | \\d+ \\d+', i_splt_regroute[[1]][1])
       x = regmatches(i_splt_regroute[[1]][1], m)
       x = trimws(x[[1]])
+      stop=c(stop,as.integer(x))
       print(x)
       print(i$text)
     }
   }
+  
+  returning= as.data.frame(start,stop)
+  return(returning)
 
 }
-translink(2020,02,01,21)#over check
-translink(2020,02,02,20)#cloverdale
-translink(2020,01,26,03)#3 at a time
+A=translink(2020,02,01,21)#over check
+B=translink(2020,02,02,20)#cloverdale
+C=translink(2020,01,26,03)#3 at a time
+C$stop
+translink(2020,02,23,15)#multiple nums
